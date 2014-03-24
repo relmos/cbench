ec80ae30094f3fb1451cb7331da6be68c89318f3
relmos <relmos@gmail.com>
2014-03-25 01:11:06 +0200

    update rails app

--- cperf/app/controllers/checks_controller.rb
+++ cperf/app/controllers/checks_controller.rb
@@ -2,9 +2,25 @@ class ChecksController < ApplicationController
 skip_before_filter :verify_authenticity_token
 respond_to :json, :html
 def index
+  @sites = ["Israel", "nydc1", "ladc1", "chidc2", "Europe"]
+  @fastly = Hash.new {|h,k| h[k] = []}
+  @edgecast = Hash.new {|h,k| h[k] = []}
+  @internap = Hash.new {|h,k| h[k] = []}
+  @akamai = Hash.new {|h,k| h[k] = []}
   @checks = Check.all
+  @sites.each do |s|
+    @fastly[s] = Check.where(cdn: 'fastly', site:s)
+    @edgecast[s] = Check.where(cdn: 'edgecast', site:s)
+    @internap[s] = Check.where(cdn: 'internap', site:s)
+    @akamai[s] = Check.where(cdn: 'akamai', site:s)
+  end
+  @fastly_total = Check.where(cdn: 'fastly')
+  @edgecast_total = Check.where(cdn: 'edgecast')
+  @internap_total = Check.where(cdn: 'internap')
+  @akamai_total = Check.where(cdn: 'akamai')
+  
+  
 end
-
 def new
   @check = Check.new
 end
@@ -21,7 +37,6 @@ def show
   @check = Check.find(params[:id])
 end
 
-private
 
 def check_params
   params.require(:check).permit(:site, :cdn, :file, :mesurement, :status)
