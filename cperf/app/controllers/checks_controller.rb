9a5cbc355f471f0fe565b54556dcaf19c9dc5cb3
relmos <relmos@gmail.com>
2014-03-26 11:12:29 +0000

    update rails + cdn script

--- cperf/app/controllers/checks_controller.rb
+++ cperf/app/controllers/checks_controller.rb
@@ -2,24 +2,27 @@ class ChecksController < ApplicationController
 skip_before_filter :verify_authenticity_token
 respond_to :json, :html
 def index
-  @sites = ["Israel", "nydc1", "ladc1", "chidc2", "Europe"]
+  @sites = ["Israel", "nydc1", "ladc1", "chidc2", "Europe", "Tokyo"]
   @fastly = Hash.new {|h,k| h[k] = []}
   @edgecast = Hash.new {|h,k| h[k] = []}
   @internap = Hash.new {|h,k| h[k] = []}
   @akamai = Hash.new {|h,k| h[k] = []}
+  @origin = Hash.new {|h,k| h[k] = []}
   @checks = Check.all
   @sites.each do |s|
     @fastly[s] = Check.where(cdn: 'fastly', site:s)
     @edgecast[s] = Check.where(cdn: 'edgecast', site:s)
     @internap[s] = Check.where(cdn: 'internap', site:s)
     @akamai[s] = Check.where(cdn: 'akamai', site:s)
+    @origin[s] = Check.where(cdn: 'origin', site:s)
   end
   @fastly_total = Check.where(cdn: 'fastly')
   @edgecast_total = Check.where(cdn: 'edgecast')
   @internap_total = Check.where(cdn: 'internap')
   @akamai_total = Check.where(cdn: 'akamai')
-  
-  
+  @origin_total = Check.where(cdn: 'origin')
+  @fastly_average = @fastly_total.average("mesurement")
+  @average = @checks.average("mesurement") 
 end
 def new
   @check = Check.new
---
+++ cperf/app/models/\
@@ -0,0 +1,3 @@
+class Check < ActiveRecord::Base
+attr_accessible :site, :cdn, :file, :mesurement, :status
+end
