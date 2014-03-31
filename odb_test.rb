77006591429092dbe82baf091138a11949f0e6d5
relmos <relmos@gmail.com>
2014-03-31 17:25:42 +0300

    add odb_test.rb

---
+++ odb_test.rb
@@ -0,0 +1,64 @@
+#!/usr/bin/ruby
+require 'rubygems'
+require 'uri'
+require 'net/http'
+require 'json'
+class TestCdn
+        def SetParameters(location)
+                @cdn = Hash.new {|h,k| h[k] = []}
+                @cdn['cdnlist'] = [ 'akamai', 'fastly', 'edgecast', 'internap', 'origin'  ]
+                @cdn['odb1'] = [ '/get?key=AYQHSUWJ8576&logStat=false&format=xml&url=http%3A//www.webx0.com/2008/06/an-alternative.html&sort=popularity&order=desc&num=4&urlType=redirect&level=debug&srcDocFetchMode=mp&recType=ALL&numVisibleRecs=1&maxNumAds=1&location=US&servePc=true&blogPost=false&wlFilter=true&readFromMemCache=true&writeToMemCache=true&recReqType=doc_recs&widgetJSId=AR_1&excludeSameSource=false&allowReplicatingRecs=false&runOflAlgs=false&readerPlatform=WEB&recsContentType=ALL&maxNumOrganicRecs=4&userDevice=desktop&recMode=rec&raterMode=none']
+                @cdn['odb2'] = [ '/get?key=AYQHSUWJ8576&logStat=false&format=xml&url=http%3A//dudihol.blogspot.co.il/2012/02/blog-post.html&sort=popularity&order=desc&num=10&urlType=redirect&level=debug&srcDocFetchMode=mp&recType=ALL&numVisibleRecs=10&maxNumAds=6&servePc=true&blogPost=false&wlFilter=true&readFromMemCache=true&writeToMemCache=true&recReqType=doc_recs&widgetJSId=AR_1&excludeSameSource=false&allowReplicatingRecs=false&runOflAlgs=false&readerPlatform=WEB&recsContentType=ALL&maxNumOrganicRecs=4&userDevice=desktop&recMode=rec&raterMode=none']
+                @cdn['odb3'] = [ '/get?key=AYQHSUWJ8576&logStat=false&format=xml&url=http%3A//oragolan.com/adhd.aspx&sort=popularity&order=desc&num=10&urlType=redirect&level=debug&srcDocFetchMode=all&recType=ALL&numVisibleRecs=10&maxNumAds=6&servePc=true&blogPost=false&wlFilter=true&readFromMemCache=true&writeToMemCache=true&recReqType=doc_recs&widgetJSId=NA&excludeSameSource=false&allowReplicatingRecs=false&runOflAlgs=false&readerPlatform=WEB&recsContentType=ALL&maxNumOrganicRecs=4&userDevice=desktop&recMode=rec&raterMode=none']
+                @cdn['odb4'] = [ '/get?key=AYQHSUWJ8576&logStat=false&format=xml&url=http%3A//edition.cnn.com/2013/12/10/tech/mars-curiosity-rover/index.html%3Fhpt%3Dhp_t4&sort=popularity&order=desc&num=10&urlType=redirect&level=debug&srcDocFetchMode=all&recType=ALL&numVisibleRecs=10&maxNumAds=6&servePc=true&blogPost=false&wlFilter=true&readFromMemCache=false&writeToMemCache=true&recReqType=doc_recs&widgetJSId=NA&excludeSameSource=false&allowReplicatingRecs=false&runOflAlgs=true&readerPlatform=MOBILE&recsContentType=ALL&maxNumOrganicRecs=4&userDevice=desktop&recMode=rec&raterMode=none']
+		@location = location 
+        end
+        def GetCdn(url)
+                @uri = URI.parse(url)
+                @http = Net::HTTP.new(@uri.host, @uri.port)
+                @http.open_timeout = 500
+                @http.read_timeout = 500
+                @request = Net::HTTP::Get.new(@uri.request_uri)
+                @response = @http.request(@request)
+        end
+  	    def PostResult(postdata)
+		             @uri = URI.parse("http://localhost:3000/odb")
+ 	               @request.body = JSON.generate(postdata)
+                 @request["Content-Type"] = "application/json"
+                 @http = Net::HTTP.new(@uri.host, @uri.port)
+                 @response = @http.start {|h| h.request(@request)}		
+	end
+        def MeasureGet()
+                @cdn['cdnlist'].each do |cdn|
+                  (1..4).each do |n|
+                      @app = "odb#{n}" 
+                      GetUrl(@cdn[@app],"utils",cdn)
+                  end
+                end
+        end
+        def GetUrl(urls,dir,cdn)
+                urls.each do |f|
+			             @begin_time = Time.now
+                   @response = GetCdn("http://odb-#{cdn}.outbrain.cc/#{dir}/#{f}")
+	              	 @code = @response.code
+	                 @end_time = Time.now
+	                 @mesurement = ((@end_time - @begin_time)*1000)
+			             @post_params = BuildRequest(cdn,f)
+			           end
+			           puts @post_params
+			           #PostResult(@post_params)
+        end
+        def BuildRequest(cdn,file)
+	       	@post_params = {
+	        :site => "#{@location}",
+       		:cdn => "#{cdn}",
+       		:file => "#{file}",
+  	      :mesurement => "#{@mesurement}",
+    	  	:status => "#{@code}"
+ 	        }
+		      return(@post_params)	
+        end
+end
+pfrtest = TestCdn.new()
+pfrtest.SetParameters(ARGV[0])
+pfrtest.MeasureGet()
